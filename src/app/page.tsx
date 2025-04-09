c9697e8e702b69853909545314951a84629ef3c0
vijayakumar1069 <vijay.r20799@gmail.com>
2025-04-10 00:44:25 +0530

    dummy content added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
+      <h1>Welcome to Expense Tracker</h1>
       <Button asChild>
         <Link href="/login">Login</Link>
       </Button>
