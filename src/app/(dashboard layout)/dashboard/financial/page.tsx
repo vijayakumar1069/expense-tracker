dd4205d35e15d531e59f86d3d4e63ff9ea466651
vijayakumar1069 <vijay.r20799@gmail.com>
2025-04-11 22:06:27 +0530

    dashbaord page force=dynamic added

--- src/app/(dashboard layout)/dashboard/financial/page.tsx
+++ src/app/(dashboard layout)/dashboard/financial/page.tsx
@@ -1,4 +1,4 @@
-// export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic";
 export default function FinancialPage() {
   return null; // This page exists just to activate the @financial parallel route
 }
